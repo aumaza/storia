b7f831e96bb8991f373c5d818806a6c29d726c36
Augusto Maza <debianmaza@gmail.com>
2021-04-17 14:19:32 -0300

    add script allow_permissions

---
+++ core/allow_permissions.sh
@@ -0,0 +1,14 @@
+#!/usr/local/bin/bash
+chmod -R 777 uploads/
+echo "Permisos en uploads cambiados Exitosamente"
+chmod -R 777 backups/
+echo "Permisos en backup cambiados Exitosamente"
+chmod -R 777 sqls/
+echo "Permisos en sqls cambiados Exitosamente"
+chmod -R 777 logs/
+echo "Permisos en logs cambiados Exitosamente"
+chmod -R 777 registro/gen_pass/
+echo "Permisos en gen_pass cambiados Exitosamente"
+chmod 777 main/
+echo "Permisos en main cambiados Exitosamente"
+
