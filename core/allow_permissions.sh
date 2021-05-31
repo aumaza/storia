e46787f6e888dfcdb8daa3ee0ea0f3386506d942
Augusto Maza <debianmaza@gmail.com>
2021-05-31 15:58:23 -0300

    fix backup database and allow_permissions

--- core/allow_permissions.sh
+++ core/allow_permissions.sh
@@ -1,14 +1,9 @@
 //#!/usr/local/bin/bash
 #!/bin/bash
 
-chmod -R 777 uploads/
-echo "Permisos en uploads cambiados Exitosamente"
-chmod -R 777 backups/
-echo "Permisos en backup cambiados Exitosamente"
+
 chmod -R 777 sqls/
 echo "Permisos en sqls cambiados Exitosamente"
-chmod -R 777 logs/
-echo "Permisos en logs cambiados Exitosamente"
 chmod -R 777 registro/gen_pass/
 echo "Permisos en gen_pass cambiados Exitosamente"
 chmod 777 manager/main/
