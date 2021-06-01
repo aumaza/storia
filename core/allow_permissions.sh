15c21608e973708fba4dcf9e78f4bf049bd68f11
Augusto Maza <debianmaza@gmail.com>
2021-06-01 13:06:28 -0300

    fix 6 backup base

--- core/allow_permissions.sh
+++ core/allow_permissions.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
 
-chmod -R 777 sqls/
+chmod -R 777 sqls/backup/
 echo "Permisos en sqls cambiados Exitosamente"
 chmod -R 777 registro/gen_pass/
 echo "Permisos en gen_pass cambiados Exitosamente"
-chmod 777 manager/main/
+chmod -R 777 manager/main/
 echo "Permisos en main cambiados Exitosamente"
 
