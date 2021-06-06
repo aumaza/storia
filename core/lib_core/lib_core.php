f229a60727d83f16ce7619cf72c6eacc498cf466
Augusto Maza <debianmaza@gmail.com>
2021-06-06 08:59:33 -0300

    update #7 6/06/2021 arreglo general

--- core/lib_core/lib_core.php
+++ core/lib_core/lib_core.php
@@ -20,7 +20,7 @@ function skeleton(){
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
 
     
-    <script src="/storia/skeleton/js/jquery-3.5.1.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/storia/skeleton/js/jquery-3.4.1.min.js"></script>
 	<script src="/storia/skeleton/js/bootstrap.min.js"></script>
 	<script src="/storia/core/lib_core/lib_scripts.js"></script>
